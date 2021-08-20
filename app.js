dc97767249d41fe1f8f0769bebf55d6189bbd59d
ELIUS <eliushossain67@gmail.com>
2021-08-20 14:44:52 +0600

    refresh js file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-/*
+
 function getProductValue(productID, price, increase) {
     const product = document.getElementById(productID + '-value')
     let productNumber = product.value
@@ -54,69 +54,5 @@ document.getElementById('casePlus-btn').addEventListener('click', function () {
 document.getElementById('caseMinus-btn').addEventListener('click', function () {
     getProductValue('case', 59, false)
 })
-*/
 
-/*
-phoneMinus-btn
-phone-value
-phonePlus-btn
-phoneAmount
 
-caseMinus-btn
-case-value
-casePlus-btn
-caseAmount
-
-subTotal-amount
-subTotal-Tax
-total-price
-*/
-function getInputValue(Input, price, increase) {
-    const productInput = document.getElementById(Input + '-value')
-    let productValue = productInput.value
-    const productInputInt = parseFloat(productValue)
-    if (increase == true) {
-        productValue = productInputInt + 1
-    } else if (productInputInt > 0) {
-        productValue = productInputInt - 1
-    }
-    productInput.value = productValue
-
-    const amount = document.getElementById(Input + 'Amount')
-    amount.innerText = productValue * price
-    totalCalculation()
-}
-// sub total function
-function getValue(product) {
-    const productValue = document.getElementById(product + '-value')
-    const productValueInt = parseInt(productValue.value)
-    return productValueInt
-
-}
-function totalCalculation() {
-    const phoneTotal = getValue('phone') * 1219
-    const caseTotal = getValue('case') * 59
-    const subTotal = phoneTotal + caseTotal
-    const Tax = subTotal / 10
-    const totalPrice = subTotal + Tax
-
-    document.getElementById('subTotal-amount').innerText = subTotal
-    document.getElementById('subTotal-Tax').innerText = Tax
-    document.getElementById('total-price').innerText = totalPrice
-
-}
-
-// phone plus minus click button
-document.getElementById('phoneMinus-btn').addEventListener('click', function () {
-    getInputValue('phone', 1219, false)
-})
-document.getElementById('phonePlus-btn').addEventListener('click', function () {
-    getInputValue('phone', 1219, true)
-})
-// case plus minus click button
-document.getElementById('caseMinus-btn').addEventListener('click', function () {
-    getInputValue('case', 59, false)
-})
-document.getElementById('casePlus-btn').addEventListener('click', function () {
-    getInputValue('case', 59, true)
-})
